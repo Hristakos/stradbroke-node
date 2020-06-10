b4fb6d5887642e2ab2d92111d9c4bb23ae3e4516
Peter Hristakos <peter.hristakos@bigpond.com>
2020-06-10 18:46:07 +1000

    removed templating

--- mail.js
+++ mail.js
@@ -2,6 +2,8 @@ const nodemailer = require('nodemailer');
 const mailGun = require('nodemailer-mailgun-transport');
 const mailGunApi = process.env.MAILGUN_API_KEY;
 const mailGunDomain = process.env.MAILGUN_DOMAIN;
+const hbs = require('nodemailer-express-handlebars');
+
 const auth = {
     auth: {
         api_key: mailGunApi,
@@ -9,19 +11,31 @@ const auth = {
     }
 }
 
-const transporter = nodemailer.createTransport(mailGun(auth));
+let transporter = nodemailer.createTransport(mailGun(auth));
 
+// transporter.use('compile', hbs({
+//     viewEngine: {
+//         extName: '.hbs',
+//         partialsDir: 'views/partials',
+//         layoutsDir: 'views/layout',
+//         defaultLayout: 'order',
+//     },
+//     viewPath: 'views/'
+// }));
 sendMail = (email, subject, text, cb) => {
     console.log(email, subject, text)
     const mailOptions = {
         from: email,
+        // to: 'paul@stradbrokeprinting.com.au',
         to: 'peter.hristakos@bigpond.com',
         subject,
-        text
+        text//,
+        //template: 'order'
     }
 
     transporter.sendMail(mailOptions, function (err, data) {
         if (err) {
+            console.log("err = " + err)
             cb(err, null)
         } else {
             cb(null, data)
